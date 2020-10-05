b904e1a23e5df748cd452602cb9dffc10765330e
Ubuntu <ubuntu@ip-172-31-30-73.us-east-2.compute.internal>
2020-10-05 22:16:39 +0000

    Created an accounts app for authentication

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+# Create your views here.
+def index(request):
+    """Return the index.html file"""
+    return render(request, 'index.html')
+    
+def logout(request):
+    """Log the user out"""
+    auth.logout(request) 
+    messages.success(request, "You have successfully been logged out!")
+    return redirect(reverse('index'))
--- issuetracker/urls.py
+++ issuetracker/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from accounts.views import index, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name="index"),
+    url(r'^accounts/logout/$', logout, name="logout")
 ]
