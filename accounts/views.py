c3185c347a1a3997811dbb5843ba844ae419680f
Ubuntu <ubuntu@ip-172-31-30-73.us-east-2.compute.internal>
2020-10-12 22:25:43 +0000

    Created a user profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 # Create your views here.
 def index(request):
@@ -57,5 +58,10 @@ def registration(request):
         registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {
         "registration_form": registration_form})
+        
+def user_profile(request):
+    """The user's profile page"""
+    user = User.objects.get(email=request.user.email)
+    return render(request, 'profile.html', {"profile": user})
 
     
--- issuetracker/urls.py
+++ issuetracker/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from accounts.views import index, logout, login, registration
+from accounts.views import index, logout, login, registration, user_profile
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^accounts/logout/$', logout, name="logout"),
     url(r'^accounts/login/$', login, name="login"),
-    url(r'^accounts/register/$', registration, name="registration")
+    url(r'^accounts/register/$', registration, name="registration"),
+    url(r'^accounts/profile/$', user_profile, name="profile")
 ]
