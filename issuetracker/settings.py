8d605bcc80c34af6ea0e8ba726a3dad9564ef38d
Ubuntu <ubuntu@ip-172-31-30-73.us-east-2.compute.internal>
2020-10-14 20:53:11 +0000

    Added a css file

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
