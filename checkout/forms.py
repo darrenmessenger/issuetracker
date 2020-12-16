8e0f19d685a963c34bb9811667642b28817b84fa
Ubuntu <ubuntu@ip-172-31-27-98.us-east-2.compute.internal>
2020-12-16 08:54:51 +0000

    made amendments to the index page and the checkout app

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
 class MakePaymentForm(forms.Form):
-    month_choices = [(i,i) for i in range(1,13)]
-    year_choices = [(i,i) for i in range(2018,2040)]
+    print("MakePaymentForm...")
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2040)]
     
     credit_card_number = forms.CharField(label='Credit Card Number', required=False)
-    cvv = forms.CharField(label ='Security Code', required=False)
-    expiry_month = forms.ChoiceField(label="Month",choices=month_choices, required=False)
-    expiry_year = forms.ChoiceField(label='year',choices=year_choices, required=False)
-    stripe_id = forms.CharField(widget=forms.HiddenInput)
+    cvv = forms.CharField(label ='Security Code (CVV)', required=False)
+    expiry_month = forms.ChoiceField(label="Month",choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='year',choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput,  required=False)
+    print("End of MakePaymentForm...")
--- checkout/views.py
+++ checkout/views.py
@@ -16,28 +16,35 @@ stripe.api_key = settings.STRIPE_SECRET
 def checkout(request):
     if request.method == "POST":
         payment_form = MakePaymentForm(request.POST)
+        print("checkout...")
 
         if  payment_form.is_valid():
+            print("Payment form is valid...")
             try:
+                print("Just about to charge the customer...")
+                print("payment_form...",payment_form)
                 customer = stripe.Charge.create(
                     amount=500,
                     currency="EUR",
-                    description="Thank you for your contribution, it will help our site tremendously" and request.user.email,
+                    description="Thank you for your contribution, it will really help our site" and request.user.email,
                     card=payment_form.cleaned_data['stripe_id']
                 )
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
             
             if customer.paid:
+                print("Customer has paid...")
                 messages.error(request, "You have successfully paid")
-                request.session['cart'] = {}
-                return redirect(reverse('products'))
+                return redirect(reverse('index'))
             else:
                 messages.error(request, "Unable to take payment")
+                return redirect(reverse('index'))
         else:
+            print("There are errors...")
             print(payment_form.errors)
             messages.error(request, "We were unable to take a payment with that card!")
     else:
+        print("Method isn't post...")
         payment_form = MakePaymentForm()
 
     return render(request, "checkout.html", {"payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
