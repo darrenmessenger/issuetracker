33a65ceba0cc99d3b8a2560055802636696c7cb2
Ubuntu <ubuntu@ip-172-31-27-98.us-east-2.compute.internal>
2020-12-16 11:00:19 +0000

    Changed the settings file so that the env module isn't required in Heroku

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import env
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
